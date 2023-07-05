54f10844f004e12130d3f4a15503a0c4dc3a8121
Schuyler Erle <schuyler@lattice.com>
2023-07-05 14:21:17 -0700

    Break out SolarReadout into separate component

--- App.tsx
+++ App.tsx
@@ -6,50 +6,12 @@ import * as Location from "expo-location";
 import { styles } from "./style";
 import { generateSolarTable, SolarTable, SolarPosition } from "./solar";
 
-export default function App() {
-  const [position, setPosition] = useState<Location.LocationObject | null>();
-  const [heading, setHeading] =
-    useState<Location.LocationHeadingObject | null>();
-  const [_errorMsg, setErrorMsg] = useState("");
-  const [solarTable, setSolarTable] = useState<SolarTable | null>();
-  const [solarPosition, setSolarPosition] = useState<SolarPosition | null>();
-
-  const headingAccuracy = ["none", "low", "med", "high"];
-
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        setErrorMsg("Permission to access location was denied");
-        return;
-      }
-
-      // Get position fix (we only need it once)
-      const lastKnownPosition = await Location.getLastKnownPositionAsync();
-      setPosition(lastKnownPosition);
-
-      if (lastKnownPosition != null) {
-        setErrorMsg("Can't determine location");
-        //console.log("lastKnownPosition", lastKnownPosition);
-        if (solarTable == null) {
-          const table = generateSolarTable(lastKnownPosition.coords);
-          setSolarTable(table);
-          //console.log("solarTable", solarTable && solarTable[0]);
-        }
-      }
-
-      // Start watching the device's compass heading
-      Location.watchHeadingAsync((heading) => {
-        setHeading(heading);
-        if (heading.trueHeading != null && heading.trueHeading != -1) {
-          const azimuth = Math.floor(heading.magHeading);
-          if (solarTable && solarTable[azimuth]) {
-            setSolarPosition(solarTable[azimuth]);
-          }
-        }
-      });
-    })();
-  }, [solarTable]);
+function SolarReadout(props: {
+  location: Location.LocationObject | undefined;
+  heading: Location.LocationHeadingObject | undefined;
+}) {
+  const [solarTable, setSolarTable] = useState<SolarTable>();
+  const [solarPosition, setSolarPosition] = useState<SolarPosition>();
 
   function padTime(n: number | undefined) {
     if (n == undefined) {
@@ -60,28 +22,67 @@ export default function App() {
 
   function formatTime(date: Date | undefined) {
     return padTime(date?.getHours()) + ":" + padTime(date?.getMinutes());
-    /*
-    if (date != undefined) {
-      return date.toLocaleTimeString() +
-        "(" +
-        (date.getTimezoneOffset() / 60).toFixed() +
-        ")";
-    }
-    */
   }
+
+  useEffect(() => {
+    console.log("location change", props.location?.coords);
+    if (!props.location) {
+      return;
+    }
+    const { latitude, longitude } = props.location.coords;
+    const table = generateSolarTable({ latitude, longitude });
+    setSolarTable(table);
+  }, [props.location]);
+
+  useEffect(() => {
+    const azimuth = props.heading?.trueHeading;
+    if (azimuth == undefined || azimuth == -1 || !solarTable) {
+      return;
+    }
+    const entry = Math.floor(azimuth);
+    setSolarPosition(solarTable[entry]);
+  }, [props.heading]);
+
+  return (
+    <View>
+      <Text style={styles.paragraph}>{formatTime(solarPosition?.time)}</Text>
+      <Text style={styles.paragraph}>↕️ {solarPosition?.elevation}º</Text>
+    </View>
+  );
+}
+
+export default function App() {
+  const [location, setLocation] = useState<Location.LocationObject>();
+  const [heading, setHeading] = useState<Location.LocationHeadingObject>();
+  const [_errorMsg, setErrorMsg] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      // Get position fix (we only need it once)
+      const lastKnownLocation = await Location.getLastKnownPositionAsync();
+      setLocation(lastKnownLocation || undefined);
+
+      // Start watching the device's compass heading
+      Location.watchHeadingAsync((heading) => {
+        setHeading(heading);
+      });
+    })();
+  }, []);
 
   return (
     <View style={styles.container}>
       <Text style={styles.paragraph}>
-        {position?.coords?.latitude.toFixed(4)}ºN &nbsp;
-        {position?.coords?.longitude.toFixed(4)}ºE
+        {location?.coords?.latitude.toFixed(4)}ºN &nbsp;
+        {location?.coords?.longitude.toFixed(4)}ºE
       </Text>
-      <Text style={styles.paragraph}>
-        {heading?.trueHeading?.toFixed(0)}º (
-        {headingAccuracy[heading?.accuracy || 0]})
-      </Text>
-      <Text style={styles.paragraph}>{formatTime(solarPosition?.time)}</Text>
-      <Text style={styles.paragraph}>↕️ {solarPosition?.elevation}º</Text>
+      <Text style={styles.paragraph}>{heading?.trueHeading?.toFixed(0)}º</Text>
+      <SolarReadout location={location} heading={heading} />
     </View>
   );
 }
